76bbc7894571918cfde7192e2c1cc20d3b81fc38
gabrielPuigMartin <gpuig@multimediatarragona.com>
2022-03-02 18:04:18 +0100

    Examen completado.

--- app/src/main/java/com/multimediatgna/goodfood/ui/main/LoginFragment.java
+++ app/src/main/java/com/multimediatgna/goodfood/ui/main/LoginFragment.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -30,13 +31,13 @@ import android.widget.TextView;
 import com.multimediatgna.goodfood.MainActivity;
 import com.multimediatgna.goodfood.R;
 
-public class LoginFragment extends Fragment implements View.OnClickListener{
+public class LoginFragment extends Fragment implements View.OnClickListener {
 
     private MainViewModel mViewModel;
-    EditText mynameedittext,mypasswdedittext;
+    EditText mynameedittext, mypasswdedittext;
     TextView mytextview;
     private Context mycontext;
-    Button mybutton;
+    Button mybutton, mybutton2;
 
 
     public static LoginFragment newInstance() {
@@ -46,7 +47,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        mycontext=context;
+        mycontext = context;
     }
 
     @Nullable
@@ -63,6 +64,9 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
         mytextview = root.findViewById(R.id.mysysteminfo);
         mybutton = root.findViewById(R.id.mysysteminfobutton);
         mybutton.setOnClickListener(this);
+        mybutton2 = root.findViewById(R.id.myphonecallbutton);
+        mybutton2.setOnClickListener(this);
+
         return root;
     }
 
@@ -76,58 +80,71 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View view) {
 
-        if(ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this.getActivity(), new String[] {Manifest.permission.READ_PHONE_STATE}, 1);
+        switch (view.getId()) {
 
-        } else {
-            String deviceId_str, softwareVersion_str, simSerialNo, voicemailNumer_str, sim_operator_Name;
-            TelephonyManager telephonyManager = (TelephonyManager) mycontext.getSystemService(mycontext.TELEPHONY_SERVICE);
-            deviceId_str = telephonyManager.getDeviceId();
-            softwareVersion_str = telephonyManager.getDeviceSoftwareVersion();
-            simSerialNo = telephonyManager.getSimSerialNumber();
-            voicemailNumer_str = telephonyManager.getVoiceMailNumber();
-            sim_operator_Name = telephonyManager.getSimOperatorName();
-            String Manufacturer_value = Build.MANUFACTURER;
-            String Brand_value = Build.BRAND;
-            String Model_value = Build.MODEL;
-            String Board_value = Build.BOARD;
-            String Hardware_value = Build.HARDWARE;
-            String Serial_nO_value = Build.SERIAL;
-            String android_id = Settings.Secure.getString(getContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-            String BootLoader_value = Build.BOOTLOADER;
-            String User_value = Build.USER;
-            String Host_value = Build.HOST;
-            String Version = Build.VERSION.RELEASE;
-            String API_level = Build.VERSION.SDK_INT + "";
-            String Build_ID = Build.ID;
-            String Build_Time = Build.TIME + "";
-            String Fingerprint = Build.FINGERPRINT;
+            case R.id.mysysteminfobutton:
+                if (ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.READ_PHONE_STATE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+                    return;
+                }
+                String deviceId_str, softwareVersion_str, simSerialNo, voicemailNumer_str, sim_operator_Name;
+                TelephonyManager telephonyManager = (TelephonyManager) mycontext.getSystemService(mycontext.TELEPHONY_SERVICE);
+                deviceId_str = telephonyManager.getDeviceId();
+                softwareVersion_str = telephonyManager.getDeviceSoftwareVersion();
+                simSerialNo = telephonyManager.getSimSerialNumber();
+                voicemailNumer_str = telephonyManager.getVoiceMailNumber();
+                sim_operator_Name = telephonyManager.getSimOperatorName();
+                String Manufacturer_value = Build.MANUFACTURER;
+                String Brand_value = Build.BRAND;
+                String Model_value = Build.MODEL;
+                String Board_value = Build.BOARD;
+                String Hardware_value = Build.HARDWARE;
+                String Serial_nO_value = Build.SERIAL;
+                String android_id = Settings.Secure.getString(getContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+                String BootLoader_value = Build.BOOTLOADER;
+                String User_value = Build.USER;
+                String Host_value = Build.HOST;
+                String Version = Build.VERSION.RELEASE;
+                String API_level = Build.VERSION.SDK_INT + "";
+                String Build_ID = Build.ID;
+                String Build_Time = Build.TIME + "";
+                String Fingerprint = Build.FINGERPRINT;
 
-            mytextview.setText(deviceId_str + "\n \n"
-                                + simSerialNo + "\n \n"
-                                + sim_operator_Name
-                                + "\n \n"
-                                + softwareVersion_str
-                                + "\n \n" + voicemailNumer_str + "\n \n"
-                                + Manufacturer_value + "\n \n"
-                                + Brand_value + "\n \n"
-                                + Model_value + "\n \n"
-                                + Board_value + "\n \n"
-                                + Hardware_value + "\n \n"
-                                + Serial_nO_value + "\n \n"
-                                + android_id + "\n \n"
-                                + BootLoader_value + "\n \n"
-                                + User_value + "\n \n"
-                                + Host_value + "\n \n"
-                                + Version + "\n \n"
-                                + API_level + "\n \n"
-                                + Build_ID + "\n \n"
-                                + Build_Time + "\n \n"
-                                + Fingerprint);
+                mytextview.setText(deviceId_str + "\n \n"
+                        + simSerialNo + "\n \n"
+                        + sim_operator_Name
+                        + "\n \n"
+                        + softwareVersion_str
+                        + "\n \n" + voicemailNumer_str + "\n \n"
+                        + Manufacturer_value + "\n \n"
+                        + Brand_value + "\n \n"
+                        + Model_value + "\n \n"
+                        + Board_value + "\n \n"
+                        + Hardware_value + "\n \n"
+                        + Serial_nO_value + "\n \n"
+                        + android_id + "\n \n"
+                        + BootLoader_value + "\n \n"
+                        + User_value + "\n \n"
+                        + Host_value + "\n \n"
+                        + Version + "\n \n"
+                        + API_level + "\n \n"
+                        + Build_ID + "\n \n"
+                        + Build_Time + "\n \n"
+                        + Fingerprint);
+
+                break;
+
+            case R.id.myphonecallbutton:
+                Intent myphoneCallIntent = new Intent(Intent.ACTION_CALL);
+                myphoneCallIntent.setData(Uri.parse(getString(R.string.callNumber)));
+                if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.CALL_PHONE}, 1);
+                    return;
+                }
+                startActivity(myphoneCallIntent);
+                break;
 
         }
-
     }
 }
